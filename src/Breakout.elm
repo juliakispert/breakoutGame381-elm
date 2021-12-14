858e6137335ff0850cfa2890a8ea78bca1e8003e
aarbay <34741065+aarbay@users.noreply.github.com>
2021-12-13 18:48:45 -0600

    bounce method stuff

--- src/Breakout.elm
+++ src/Breakout.elm
@@ -2,15 +2,15 @@
 -- The classic game of Asteroids, minus the pesky problem of dying.
 --
 -- Author: Paul Cantrell
--- documentation, comments, random for the ball + angle of the ball first drop, 
+-- documentation, comments, random for the ball + angle of the ball first drop,
 -- computer.screen dimensions, brick colors, brick collisions, win/lose status,
+
 module Breakout exposing (game)
 
 import Playground exposing (..)
 import Array
 import Random
 import Set
-import Html exposing (object)
 
 game =
   { initialState = initialState
@@ -22,6 +22,7 @@ game =
 
 ballRadius = 10
 ballColor = black
+initialBallSpeed = 0.75
 
 paddleWidth = 200
 paddleHeight = 20
@@ -30,9 +31,8 @@ paddleColor = black
 brickColors = Array.fromList [rgb 247 0 0, rgb 255 178 46, rgb 247 240 40, rgb 42 252 0, rgb 0 248 252, rgb 252 0 239, rgb 255 171 186]
 brickWidth = 70
 brickHeight = 30
-
 brickColor: Color
-brickColor = red 
+brickColor = red
 
 screenWidth = 1280
 smallerScreenWidth = 638
@@ -46,19 +46,18 @@ type alias Ball =
   , dx : Float
   , dy : Float
   , shape : Shape
-  , lives : Int 
-  , alive : Bool
   }
-type alias Paddle = 
+
+type alias Paddle =
   { x : Float
   , y : Float
   , shape : Shape
   }
 
-type alias Brick = 
+type alias Brick =
   { x : Float
   , y : Float
-  , color : Color
+  , color: Color
   , shape : Shape
   , hit : Bool
   }
@@ -77,8 +76,6 @@ initialState =
       , dx = 10
       , dy = 10
       , shape = circle ballColor ballRadius
-      , lives = 3
-      , alive = True
       }
   , paddle =
       { x = 0
@@ -88,14 +85,22 @@ initialState =
   , bricks = (brickIndices |> List.map getBrick)
   }
 
-brickIndices = 
-  [ (0,0) , (1,0) , (2,0) , (3,0) , (4,0) , (5,0) , (6,0) 
+brickIndices =
+  [ (0,0) , (1,0) , (2,0) , (3,0) , (4,0) , (5,0) , (6,0)
   , (0,1) , (1,1) , (2,1) , (3,1) , (4,1) , (5,1) , (6,1)
   , (0,2) , (1,2) , (2,2) , (3,2) , (4,2) , (5,2) , (6,2)
   , (0,3) , (1,3) , (2,3) , (3,3) , (4,3) , (5,3) , (6,3)
   , (0,4) , (1,4) , (2,4) , (3,4) , (4,4) , (5,4) , (6,4)
   , (0,5) , (1,5) , (2,5) , (3,5) , (4,5) , (5,5) , (6,5)
   , (0,6) , (1,6) , (2,6) , (3,6) , (4,6) , (5,6) , (6,6) ]
+
+{-| playingAround : Int -> Int -> List (Int, Int)
+playingAround xs ys =
+  List.range 0 ys
+    |> List.concatMap (\y ->
+      List.range 0 xs
+        |> List.map (\x -> (x, y)))
+-}
 
 getBrick: (Float, Float) -> Brick
 getBrick (xIndex, yIndex) =
@@ -119,6 +124,7 @@ view computer model =
     ++ [model.ball      |> viewBall ballColor ballRadius]
     ++ [model.paddle    |> viewPaddle paddleColor paddleWidth paddleHeight]
     ++ (model.bricks    |> List.map (viewBrick brickWidth brickHeight))
+    --++ [words black (Debug.toString(playingAround 6 6)) |> move 0 0]
 
 viewBall : Color -> Float -> Ball -> Shape
 viewBall color radius obj =
@@ -130,7 +136,6 @@ viewPaddle color width height obj =
   rectangle color width height
     |> move obj.x obj.y
 
-
 viewBrick : Float -> Float -> Brick -> Shape
 viewBrick width height obj =
   rectangle obj.color width height
@@ -141,8 +146,7 @@ viewBrick width height obj =
 update computer model =
   model
     |> handleMotion computer
-    |> checkBrickCollisions  
-    |> handleGameLogic computer
+    |> checkBrickCollisions
 
 handleMotion computer model =
   { model
@@ -150,92 +154,103 @@ handleMotion computer model =
    , paddle = (movePaddle computer) model.paddle
   }
 
-
-handleGameLogic computer model =
-  { model
-   | ball = (checkWinLose computer model.ball)
-  }
-
 moveBall : Computer -> Model -> Ball -> Ball
 moveBall computer model ball =
   { ball
     | x = ball.x + ball.dx
     , y = ball.y + ball.dy
-    , dx = horizontalBounce computer model
-    , dy = verticalBounce computer model
-    -- , lives = lifeLogic computer model.ball 
+    , dx = ball.dx
+    , dy = ball.dy
     }
-
-
-checkWinLose : Computer -> Ball -> Ball
-checkWinLose computer ball = 
- { ball
-    | lives = lifeLogic computer ball
-    ,  alive = ballAlive computer ball
-    }
-
-lifeLogic computer ball  =
-  let
-      lifeReduction = ball.y + ballRadius >= computer.screen.bottom -- check if ball's position is at the bottom of the screen
-  in
-    if (lifeReduction) then 
-      ball.lives - 1
-  
-    else
-      ball.lives
-
-
-ballAlive : Computer -> Ball -> Bool
-ballAlive computer ball  =
-  let
-      lifeReduction = ball.y + ballRadius >= computer.screen.bottom -- check if ball's position is at the bottom of the screen
-  in
-    if (lifeReduction) then 
-      not ball.alive
-  
-    else
-    ball.alive
 
 brickIsNotHit brick =
   not brick.hit
 
 checkBrickCollisions model =
-  { model 
+  { model
     | bricks = List.filter brickIsNotHit model.bricks}
-
 
 movePaddle : Computer -> Paddle -> Paddle
 movePaddle computer obj =
   { obj | x = computer.mouse.x }
 
 -- this is a helper function written by Julia Kispert to have the ball bounce off the edges of the screen
-horizontalBounce: Computer -> Model -> Number
-horizontalBounce computer model =
-  if ((model.ball.x + model.ball.dx - ballRadius < computer.screen.left) || (model.ball.x + model.ball.dx + ballRadius > computer.screen.right))
-  then
-    model.ball.dx * (-1)
-  else
-    model.ball.dx
+bounceOffScreen computer ball =
+  let
+    nextX = ball.x + ball.dx
+    nextY = ball.y + ball.dy
 
-  
-verticalBounce: Computer -> Model -> Number
-verticalBounce computer model =
-  if ((model.ball.y + model.ball.dy + ballRadius > computer.screen.top))
-  then
-    model.ball.dy * (-1)
-  else
-    if (model.ball.y + model.ball.dy + ballRadius < computer.screen.bottom) then 
-      0
-    else
-      bounce model
-
-bounce : Model -> Float
-bounce model = 
-  let 
-    xCollision = (model.ball.x + model.ball.dx > model.paddle.x - paddleWidth/2 && model.ball.x + model.ball.dx < model.paddle.x + paddleWidth/2)
-    yCollision = (model.ball.y + model.ball.dy - ballRadius == model.paddle.y + paddleHeight/2)
+    leftBound = computer.screen.left + ballRadius
+    rightBound = computer.screen.right - ballRadius
+    topBound = computer.screen.top - ballRadius
   in
-    if xCollision && yCollision then 
-      model.ball.dy * (-1)
-    else 
-      model.ball.dy
+    if ((nextX < leftBound) || (nextX > rightBound))
+    then
+      switchDx ball
+    else if (nextY > topBound)
+    then
+      switchDy ball
+    else
+      ball
+
+bounceOffPaddle paddle ball =
+  let
+    nextX = ball.x + ball.dx
+    nextY = ball.y + ball.dy
+
+    paddleLeft = paddle.x - paddleWidth/2
+    paddleRight = paddle.x + paddleWidth/2
+    paddleTop = paddle.y + paddleHeight/2 + ballRadius
+  in
+    if ((nextX >= paddleLeft && nextX <= paddleRight) && (nextY <= paddleTop))
+    then
+      switchDy ball
+    else
+      ball
+
+bounceOffBrick computer ball brick =
+  let
+    ballBottom = ball.y + ball.dy - ballRadius
+    ballTop = ball.y + ball.dy + ballRadius
+    ballLeft= ball.x + ball.dx - ballRadius
+    ballRight = ball.x + ball.dx + ballRadius
+
+    brickBottom = brick.y - brickHeight/2
+    brickTop = brick.y + brickHeight/2
+    brickLeft = brick.x - brickWidth/2
+    brickRight = brick.x + brickWidth/2
+
+    xRange = (ballRight >= brickLeft && ballLeft <= brickRight)
+    yRange = (ballTop >= brickBottom && ballBottom <= brickTop)
+
+    bottomCollision = (xRange && (ballTop >= brickBottom))
+    topCollision = (xRange && (ballBottom <= brickTop))
+    leftCollision = (yRange && (ballRight >= brickLeft))
+    rightCollision = (yRange && (ballLeft <= brickRight))
+  in
+    if (topCollision || bottomCollision)
+    then
+      brickIsHit brick
+      switchDy ball
+    else if (rightCollision || leftCollision)
+    then
+      brickIsHit brick
+      switchDx ball
+    else
+      ball
+
+switchDx ball =
+  { ball
+    | dx = ball.dx * (-1) }
+
+switchDy ball =
+  { ball
+    | dy = ball.dy * (-1) }
+
+brickIsHit brick =
+  { brick
+    | hit = True }
+
+--bounce : Float -> Bool -> Bool -> Float
+-- Created by Paul Cantrel via Asteroid Assignment
+-- have to change it so ball falls off screen
