fdc42372cb06acff14c430a61ab0c44bd13e8f2d
aarbay <34741065+aarbay@users.noreply.github.com>
2021-12-14 09:47:19 -0600

    latest version + cleanup

--- src/Breakout.elm
+++ src/Breakout.elm
@@ -31,9 +31,9 @@ paddleColor = black
 brickColors = Array.fromList [rgb 247 0 0, rgb 255 178 46, rgb 247 240 40, rgb 42 252 0, rgb 0 248 252, rgb 252 0 239, rgb 255 171 186]
 brickWidth = 70
 brickHeight = 30
-screenWidth = 1280
-smallerScreenWidth = 638
-screenHeight = 559
+
+screenWidth = 600
+screenHeight = 500
 
 ------ MODEL ------
 
@@ -80,36 +80,28 @@ initialState =
       , y = -250
       , shape = rectangle paddleColor paddleWidth paddleHeight
       }
-  , bricks = (brickIndices |> List.map getBrick)
+  , bricks = (brickCoordinates 6 6 |> List.map getBrick)
   }
 
-brickIndices =
-  [ (0,0) , (1,0) , (2,0) , (3,0) , (4,0) , (5,0) , (6,0)
-  , (0,1) , (1,1) , (2,1) , (3,1) , (4,1) , (5,1) , (6,1)
-  , (0,2) , (1,2) , (2,2) , (3,2) , (4,2) , (5,2) , (6,2)
-  , (0,3) , (1,3) , (2,3) , (3,3) , (4,3) , (5,3) , (6,3)
-  , (0,4) , (1,4) , (2,4) , (3,4) , (4,4) , (5,4) , (6,4)
-  , (0,5) , (1,5) , (2,5) , (3,5) , (4,5) , (5,5) , (6,5)
-  , (0,6) , (1,6) , (2,6) , (3,6) , (4,6) , (5,6) , (6,6) ]
-
-{-| playingAround : Int -> Int -> List (Int, Int)
-playingAround xs ys =
+brickCoordinates: Int -> Int -> List (Float, Float)
+brickCoordinates xs ys =
   List.range 0 ys
-    |> List.concatMap (\y ->
-      List.range 0 xs
-        |> List.map (\x -> (x, y)))
--}
+    |> List.map toFloat
+      |> List.concatMap (\y ->
+         List.range 0 xs
+          |> List.map toFloat
+            |> List.map (\x -> (x, y)))
 
 getBrick: (Float, Float) -> Brick
 getBrick (xIndex, yIndex) =
   let
-    x = -smallerScreenWidth / 2
+    x = -screenWidth / 2
     y = screenHeight / 1.5
-    xPadding = smallerScreenWidth / 50
+    xPadding = screenWidth / 50
     yPadding = screenHeight / 75
   in
-    { x = x + (xIndex+1)*brickWidth + xIndex*xPadding
-    , y = y - (yIndex+3)*brickHeight - yIndex*yPadding
+    { x = x + (xIndex + 1) * brickWidth + xIndex * xPadding
+    , y = y - (yIndex + 3) * brickHeight - yIndex * yPadding
     , color = (Maybe.withDefault red (Array.get (yIndex |> round) brickColors))
     , shape = rectangle (Maybe.withDefault red (Array.get (yIndex |> round) brickColors)) brickWidth brickHeight
     , hit = False
@@ -119,25 +111,24 @@ getBrick (xIndex, yIndex) =
 
 view computer model =
   [rectangle white computer.screen.width computer.screen.height]
-    ++ [model.ball      |> viewBall ballColor ballRadius]
-    ++ [model.paddle    |> viewPaddle paddleColor paddleWidth paddleHeight]
-    ++ (model.bricks    |> List.map (viewBrick brickWidth brickHeight))
-    --++ [words black (Debug.toString(playingAround 6 6)) |> move 0 0]
+    ++ [model.ball      |> viewBall]
+    ++ [model.paddle    |> viewPaddle]
+    ++ (model.bricks    |> List.map viewBrick)
 
-viewBall : Color -> Float -> Ball -> Shape
-viewBall color radius obj =
-  circle color radius
-    |> move obj.x obj.y
+viewBall : Ball -> Shape
+viewBall ball =
+  circle ballColor ballRadius
+    |> move ball.x ball.y
 
-viewPaddle : Color -> Float -> Float -> Paddle -> Shape
-viewPaddle color width height obj =
-  rectangle color width height
-    |> move obj.x obj.y
+viewPaddle : Paddle -> Shape
+viewPaddle paddle =
+  rectangle paddleColor paddleWidth paddleHeight
+    |> move paddle.x paddle.y
 
-viewBrick : Float -> Float -> Brick -> Shape
-viewBrick width height obj =
-  rectangle obj.color width height
-    |> move obj.x obj.y
+viewBrick : Brick -> Shape
+viewBrick brick =
+  rectangle brick.color brickWidth brickHeight
+    |> move brick.x brick.y
 
 ------ UPDATE ------
 
@@ -145,14 +136,13 @@ update computer model =
   model
     |> handleMotion computer
     |> checkBrickCollisions
+    |> removeBricks
 
 handleMotion computer model =
   { model
    | ball = (moveBall computer model model.ball)
-   , paddle = (movePaddle computer) model.paddle
+   , paddle = (movePaddle computer model.paddle)
   }
-
-
 
 moveBall : Computer -> Model -> Ball -> Ball
 moveBall computer model ball =
@@ -162,6 +152,7 @@ moveBall computer model ball =
     , dx = horizontalBounce computer model
     , dy = verticalBounce computer model
     }
+
 -- this is a helper function written by Julia Kispert to have the ball bounce off the edges of the screen
 horizontalBounce: Computer -> Model -> Number
 horizontalBounce computer model =
@@ -180,10 +171,10 @@ verticalBounce computer model =
     if (model.ball.y + model.ball.dy + ballRadius < computer.screen.bottom) then 
       0
     else
-      bounce model
+      bounceOffPaddle model
 
-bounce : Model -> Float
-bounce model = 
+bounceOffPaddle : Model -> Float
+bounceOffPaddle model = 
   let 
     xCollision = (model.ball.x + model.ball.dx > model.paddle.x - paddleWidth/2 && model.ball.x + model.ball.dx < model.paddle.x + paddleWidth/2)
     yCollision = (model.ball.y + model.ball.dy - ballRadius == model.paddle.y + paddleHeight/2)
@@ -193,52 +184,22 @@ bounce model =
     else 
       model.ball.dy
 
-brickIsNotHit brick =
-  not brick.hit
+movePaddle : Computer -> Paddle -> Paddle
+movePaddle computer paddle =
+  { paddle | x = computer.mouse.x }
 
 checkBrickCollisions model =
   { model
     | bricks = List.filter brickIsNotHit model.bricks}
 
-movePaddle : Computer -> Paddle -> Paddle
-movePaddle computer obj =
-  { obj | x = computer.mouse.x }
+brickIsNotHit brick =
+  not brick.hit
 
--- this is a helper function written by Julia Kispert to have the ball bounce off the edges of the screen
-bounceOffScreen computer ball =
-  let
-    nextX = ball.x + ball.dx
-    nextY = ball.y + ball.dy
+removeBricks model =
+  { model
+    | bricks = (model.bricks |> List.map (removeBrick model.ball)) }
 
-    leftBound = computer.screen.left + ballRadius
-    rightBound = computer.screen.right - ballRadius
-    topBound = computer.screen.top - ballRadius
-  in
-    if ((nextX < leftBound) || (nextX > rightBound))
-    then
-      switchDx ball
-    else if (nextY > topBound)
-    then
-      switchDy ball
-    else
-      ball
-
-bounceOffPaddle ball paddle =
-  let
-    nextX = ball.x + ball.dx
-    nextY = ball.y + ball.dy
-
-    paddleLeft = paddle.x - paddleWidth/2
-    paddleRight = paddle.x + paddleWidth/2
-    paddleTop = paddle.y + paddleHeight/2 + ballRadius
-  in
-    if ((nextX >= paddleLeft && nextX <= paddleRight) && (nextY <= paddleTop))
-    then
-      switchDy ball
-    else
-      ball
-
-bounceOffBrick computer ball brick =
+removeBrick ball brick =
   let
     ballBottom = ball.y + ball.dy - ballRadius
     ballTop = ball.y + ball.dy + ballRadius
@@ -252,35 +213,93 @@ bounceOffBrick computer ball brick =
 
     xRange = (ballRight >= brickLeft && ballLeft <= brickRight)
     yRange = (ballTop >= brickBottom && ballBottom <= brickTop)
-
-    bottomCollision = (xRange && (ballTop >= brickBottom))
-    topCollision = (xRange && (ballBottom <= brickTop))
-    leftCollision = (yRange && (ballRight >= brickLeft))
-    rightCollision = (yRange && (ballLeft <= brickRight))
   in
-    if (topCollision || bottomCollision)
+    if (xRange && yRange)
     then
-  --    brickIsHit brick
-      switchDy ball
-    else if (rightCollision || leftCollision)
-    then
- --     brickIsHit brick
-      switchDx ball
+      brickGetsHit brick
     else
-      ball
+      brick
 
-switchDx ball =
-  { ball
-    | dx = ball.dx * (-1) }
-
-switchDy ball =
-  { ball
-    | dy = ball.dy * (-1) }
-
-brickIsHit brick =
+brickGetsHit: Brick -> Brick
+brickGetsHit brick =
   { brick
     | hit = True }
 
---bounce : Float -> Bool -> Bool -> Float
--- Created by Paul Cantrel via Asteroid Assignment
--- have to change it so ball falls off screen
+-- bounceOffBricks bricks ball =
+--   bricks |> List.map (bounceOffBrick ball)
+
+-- bounceOffBrick ball brick =
+--   let
+--     ballBottom = ball.y + ball.dy - ballRadius
+--     ballTop = ball.y + ball.dy + ballRadius
+--     ballLeft= ball.x + ball.dx - ballRadius
+--     ballRight = ball.x + ball.dx + ballRadius
+
+--     brickBottom = brick.y - brickHeight/2
+--     brickTop = brick.y + brickHeight/2
+--     brickLeft = brick.x - brickWidth/2
+--     brickRight = brick.x + brickWidth/2
+
+--     xRange = (ballRight >= brickLeft && ballLeft <= brickRight)
+--     yRange = (ballTop >= brickBottom && ballBottom <= brickTop)
+  
+--     bottomCollision = (xRange && (ballTop >= brickBottom))
+--     topCollision = (xRange && (ballBottom <= brickTop))
+--     leftCollision = (yRange && (ballRight >= brickLeft))
+--     rightCollision = (yRange && (ballLeft <= brickRight))
+--   in
+--     if (topCollision || bottomCollision)
+--     then
+--       0
+--     else if (rightCollision || leftCollision)
+--     then
+--       1
+--     else
+--       2
+
+-- bounceOffScreen computer ball =
+--   let
+--     nextX = ball.x + ball.dx
+--     nextY = ball.y + ball.dy
+
+--     minX = computer.screen.left + ballRadius
+--     maxX = computer.screen.right - ballRadius
+--     maxY = computer.screen.top - ballRadius
+
+--     xInBounds = (nextX >= minX && nextX <= maxX)
+--     yInBounds = nextY <= maxY
+--   in
+--     if (xInBounds && yInBounds) then
+--       { ball
+--         | x = nextX
+--         , y = nextY }
+--     else if (xInBounds) then
+--       { ball
+--         | x = nextX
+--         , y = nextY 
+--         , dy = -ball.dy }
+--     else
+--       { ball
+--         | x = nextX
+--         , y = nextY 
+--         , dx = -ball.dx }
+
+-- bounceOffPaddle paddle ball =
+--   let
+--     nextX = ball.x + ball.dx
+--     nextY = ball.y + ball.dy
+
+--     minX = paddle.x - paddleWidth/2
+--     maxX = paddle.x + paddleWidth/2
+--     maxY = paddle.y + paddleHeight/2 + ballRadius
+
+--     xInBounds = (nextX >= minX && nextX <= maxX)
+--     yInBounds = nextY <= maxY
+--   in
+--     if (xInBounds && yInBounds) then
+--       { ball
+--           | x = nextX
+--           , y = nextY 
+--           , dy = -ball.dy }
+--     else
+--       ball
